17fe64c7eee8ef029d0679ff60fdcbeb1707e73d
AnJaeSeongS2 <jindam91@naver.com>
2021-06-09 00:05:53 +0900

    [axios] cors proxy pass

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,12 @@
 <script>
 export default {
   async created() {
-    const response = await this.$axios.get("https://cors-anywhere.herokuapp.com/https://news.google.com/topstories?hl=ko&gl=KR&ceid=KR:ko")
+  const response = await this.$axios.get("https://cors-anywhere.herokuapp.com/https://bbs.ruliweb.com/best/humor/now?custom_list=best")
+    // naver news
+    // const response = await this.$axios.get("https://cors-anywhere.herokuapp.com/https://news.naver.com/main/clusterArticles.nhn?id=c_202106081650_00000324&mode=LSD&mid=shm&sid1=100&oid=001&aid=0012446320")
+    // google news (not working)
+    // const response = await this.$axios.get("https://cors-anywhere.herokuapp.com/https://news.google.com/topstories?hl=ko&gl=KR&ceid=KR:ko")
+    // const response = await this.$axios.get("https://cors-anywhere.herokuapp.com/https://lowell-dev.tistory.com/51")
     console.log(response.data)
   }
 }
